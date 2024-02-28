a5da10156aa5489b1169c35356573665f2411af9
T-cordoba <tommy.cormo@gmail.com>
2024-02-28 13:17:50 -0500

    Correction

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from clases.clases import Student
 
-n = int(input("Ingrese una nota"))
-s: Student = Student("Tomas", 18, [])
+students_data = [
+    {"name": "Estudiante1", "age": 20, "grades": [90, 85, 92]},
+    {"name": "Estudiante2", "age": 22, "grades": [78, 88, 95]},
+    {"name": "Estudiante3", "age": 21, "grades": [85, 90, 88]},
+]
 
-Student.add_grade(n)
 
-print(s)
