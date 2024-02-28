5744869d8cf0452dd22815e208b13ef7064a556b
T-cordoba <tommy.cormo@gmail.com>
2024-02-28 13:21:31 -0500

    add students_data

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from clases.clases import Student
 
 students_data = [
-    {"name": "Estudiante1", "age": 20, "grades": [90, 85, 92]},
-    {"name": "Estudiante2", "age": 22, "grades": [78, 88, 95]},
-    {"name": "Estudiante3", "age": 21, "grades": [85, 90, 88]},
+    {"name": "Estudiante1", "age": 20, "grades": [4.0, 3.7, 4.8]},
+    {"name": "Estudiante2", "age": 22, "grades": [3.0, 3.6, 2.7]},
+    {"name": "Estudiante3", "age": 21, "grades": [1.5, 4.1, 5.0]},
 ]
 
 
