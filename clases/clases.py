d8dce7f5f04914c86692fd3b10d17da775042ff9
T-cordoba <tommy.cormo@gmail.com>
2024-02-28 13:17:43 -0500

    Correction and add average_grade

--- clases/clases.py
+++ clases/clases.py
@@ -1,8 +1,11 @@
 class Student:
-    def __init__(self,  name: str, age: int, grades: list):
+    def __init__(self,  name: str, age: int, grades: list[float]):
         self.name: str = name
         self.age: int = age
-        self.grades: list = grades
+        self.grades: list[float] = grades
 
-    def add_grade(self, n: int):
-        self.grades = self.grades.append(n)
+    def add_grade(self, grade: float):
+        self.grades.append(grade)
+
+    def average_grade(self):
+        return sum(self.grades) / len(self.grades)
